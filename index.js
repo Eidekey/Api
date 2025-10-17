ddc4b8219204ed7b8111074fa2fca1839a7bd08f
Eidekey <157548831+Eidekey@users.noreply.github.com>
2025-10-17 12:39:17 -0500

    index.js

--- index.js
+++ index.js
@@ -13,8 +13,22 @@ app.post("/saludo", (req, res) => {
   console.log("Tiene ad_accounts?:", req.body.hasOwnProperty("ad_accounts"));
   console.log("Valor de ad_accounts:", req.body.ad_accounts);
 
+  const token = "EAAWKn4ZCjg3ABPvM6yNdpT3m0YC4NlOZBqnk6NwP3357JZBlLVtfvSggaJde3bkislJxnIjagEGl5TZCgh2ZB9wFBHtBf7UxkaU90P3g7LMOpkv90ByZC4ODy83ebh4x7egB6vqsHZCecKWGwgAuKLHDOflDLKwlWMNZBv5bQgpCGvv7JlPkUCa4PJlRIRYvfeL5SAZDZD"
   const ad_accounts = req.body?.ad_accounts ?? [];
-  res.json({ count: ad_accounts.length });
+  const batch = []
+  const url_base = "https://graph.facebook.com/v23.0/"
+  ad_accounts.forEach((ad_account)=>{
+    batch.push({method: "GET", relative_url: `${ad_account}/campaigns?fields=name,status`});
+  })
+
+  res = fecth(url_base,{
+    method: "POST",
+    payload: {
+      access_tocken: token,
+      batch: JSON.stringify(batch)
+  })
+  
+  //res.json({ count: ad_accounts.length });
 });
 
 const PORT = process.env.PORT || 8080;
