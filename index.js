1c6e5999f4efc57304fbd9c9aef7f54951c05731
Eidekey <157548831+Eidekey@users.noreply.github.com>
2025-10-17 11:53:06 -0500

    index.js

--- index.js
+++ index.js
@@ -7,18 +7,15 @@ app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-app.post("/saludo", express.json(), (req, res) => {
+app.post("/saludo", (req, res) => {
   console.log("Body completo recibido:", req.body);
   console.log("Tipo de body:", typeof req.body);
   console.log("Tiene ad_accounts?:", req.body.hasOwnProperty("ad_accounts"));
+  console.log("Valor de ad_accounts:", req.body.ad_accounts);
 
   const ad_accounts = req.body?.ad_accounts ?? [];
-  console.log("Valor de ad_accounts:", ad_accounts);
-
   res.json({ count: ad_accounts.length });
 });
-
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor activo en puerto ${PORT}`));
