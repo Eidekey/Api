3f5a16fe50b8351c0b08654f647ac9b3444d4666
Eidekey <157548831+Eidekey@users.noreply.github.com>
2025-10-20 10:43:39 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post("/saludo", async (req, res) => {
       }),
     });
 
-    console.log("Response sin pasar a JSON: ",response,null,2)
+    console.log("Response sin pasar a JSON: " + response)
 
     const data = await response.json();
 
