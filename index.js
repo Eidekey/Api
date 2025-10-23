c5d6be983de6a5ac305dd9ff630c293d940c0bd5
Eidekey <juanpabloyepes17082000@gmail.com>
2025-10-23 17:23:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ app.post("/saludo", async (req, res) => {
       for (const chunk of chunks) {
         const batch = chunk.map(acc => ({
           method: "GET",
-          relative_url: `act_${acc.id}/insights?fields=account_id,account_name,impressions,reach,spend,date_start,date_stop&time_range[since]=${since}&time_range[until]=${until}&level=account`
+          relative_url: `${acc.id}/insights?fields=account_id,account_name,impressions,reach,spend,date_start,date_stop&time_range[since]=${since}&time_range[until]=${until}&level=account`
         }));
 
         const batchUrl = `https://graph.facebook.com/v21.0/?access_token=${token}`;
@@ -260,7 +260,7 @@ app.post("/saludo", async (req, res) => {
         // Test de una cuenta específica
         const testAccount = allAccounts[0]?.id;
         if (testAccount) {
-          const testUrl = `https://graph.facebook.com/v21.0/act_${testAccount}/insights?fields=account_id,account_name,impressions,reach,spend,date_start,date_stop&time_range[since]=${since}&time_range[until]=${until}&level=account&access_token=${token}`;
+          const testUrl = `https://graph.facebook.com/v21.0/${testAccount}/insights?fields=account_id,account_name,impressions,reach,spend,date_start,date_stop&time_range[since]=${since}&time_range[until]=${until}&level=account&access_token=${token}`;
           const testResp = await fetch(testUrl);
           const testData = await testResp.json();
           console.log("🔍 Test insights 1 cuenta:", testData);
